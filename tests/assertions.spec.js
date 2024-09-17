2e9ab27451c911327108609e212f9e3c8c9e0d37
TessaWin <tewinterova@seznam.cz>
2024-09-17 10:46:19 +0200

    Add files via upload

--- tests/assertions.spec.js
+++ tests/assertions.spec.js
@@ -8,19 +8,12 @@ test('getAttributeValue Test', async({page}) => {
     await expect(page).toHaveURL('https://demo.applitools.com/')
 
     await page.locator('[placeholder="Enter your username"]').fill('Tereza')
-    
     await expect(page.locator('input[placeholder="Enter your username"]')).toHaveValue('Tereza')
 
     await page.locator('[placeholder="Enter your password"]').fill('1234')
+    await expect(page.locator('input[placeholder="Enter your password"]')).toHaveValue('1234')
    
     await page.locator ('text=Sign in').click()
-
     await expect(page.getByText('Jack Gomez')).toBeVisible()
-
-    //await page.pause()
-
-    //const customerName = page.locator('class')
-    //await expect(customerName).toHaveAttribute('class', 'Jack Gomez')
-
 
 })
